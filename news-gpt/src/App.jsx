18243ad9d3dc76c49038fcd11444871e6063482b
Emmanuel289 <emmirald@gmail.com>
2023-05-04 08:52:24 -0600

    A template list of articles

--- news-gpt/src/App.jsx
+++ news-gpt/src/App.jsx
@@ -5,6 +5,25 @@ const welcomeMessage = {
   'title': 'News Room',
 };
 
+const authorList = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
+
 function getMessage(message){
   return `${message.greeting} to your ${message.title}`;
   
@@ -16,9 +35,27 @@ function App() {
   return (
    <div>
      {!welcomeMessage.greeting || !welcomeMessage.title ? <p>Message must have a greeting and a title</p> : <h1>{getMessage(welcomeMessage)}</h1>}
+
+     {/* <p><em>In the textbox below, type @ followed by a keyword for a news category (e.g. @sports to get the latest sports news)</em></p> */}
     
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr/>
+
+      <ul>
+        {authorList.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+            </li>
+          );
+        })}
+      </ul>
    </div>
   );
 }
